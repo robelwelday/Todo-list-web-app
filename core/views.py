6e815a33724c9d0d3575bef287cbe2d700a23425
Robel welday <Robelwelday16@gmail.com>
2025-06-18 16:17:42 +0300

    add search

--- core/views.py
+++ core/views.py
@@ -39,14 +39,23 @@ class RegisterPage(FormView):
 
 
 
-class Tasklist(LoginRequiredMixin,ListView):
-    model=Task
-    template_name='task_list.html'    
-    context_object_name='tasks'
+class Tasklist(LoginRequiredMixin, ListView):
+    model = Task
+    template_name = 'task_list.html'
+    context_object_name = 'tasks'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(completed=False).count()
+        
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                taskname__icontains=search_input)  # Use 'taskname' instead of 'title'
+
+        context['search_input'] = search_input
+
         return context
 
 class taskdetail(LoginRequiredMixin,DetailView):
